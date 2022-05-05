e6c06f2966dd9f75ba0dfeb226f268132fd8fcf4
JustinMelore <88466579+JustinMelore@users.noreply.github.com>
2022-05-05 16:34:23 -0700

    Created a new javascript file to auto-generate the individual game pages

---
+++ js/game.js
@@ -0,0 +1,8 @@
+//Object that stores all of the information for the auto-generated page
+class GamePage {
+    constructor(name, description, progress) {
+        this.name = name;
+        this.description = description;
+        this.progress = progress;
+    }
+}
