41b48f4c8776c5fe33456ee06eff6ca34d872c7e
JustinMelore <88466579+JustinMelore@users.noreply.github.com>
2022-05-05 18:10:10 -0700

    Updated the layout of the individual game page

--- js/game.js
+++ js/game.js
@@ -6,3 +6,11 @@ class GamePage {
         this.progress = progress;
     }
 }
+
+//Map that holds all of the information for all 3 game page
+const gamesList = new Map([
+    ["voidcage",new GamePage("voidcage",`Play as a lone crewman on the the interstellar transport ship, the VOIDCAGE, which is on route to transport a collection of alien lifeforms from across the galaxy. As a mysterious breach rocks the ship, you will have to struggle to survive and find a way off the ship as hostile creatures take over and the VOIDCAGE begins to collapse. Arm yourself with scattered tools from the crew and learn to conquer the intergalactic food chain before you're consumed, either by the aliens, or the void of space.`,"Out Now!")],
+    ["placeholder1",""],
+    ["placeholder2",""]
+]);
+
