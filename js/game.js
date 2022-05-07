1a167ec3e0f3e2f84a164aca6d18afb4f8b25655
JustinMelore <88466579+JustinMelore@users.noreply.github.com>
2022-05-06 18:21:39 -0700

    Began working on a function that will generate the individual games' page

--- js/game.js
+++ js/game.js
@@ -10,7 +10,14 @@ class GamePage {
 //Map that holds all of the information for all 3 game page
 const gamesList = new Map([
     ["voidcage",new GamePage("voidcage",`Play as a lone crewman on the the interstellar transport ship, the VOIDCAGE, which is on route to transport a collection of alien lifeforms from across the galaxy. As a mysterious breach rocks the ship, you will have to struggle to survive and find a way off the ship as hostile creatures take over and the VOIDCAGE begins to collapse. Arm yourself with scattered tools from the crew and learn to conquer the intergalactic food chain before you're consumed, either by the aliens, or the void of space.`,"Out Now!")],
-    ["placeholder1",""],
-    ["placeholder2",""]
+    ["13",""],
+    ["hylophobia",""]
 ]);
 
+//Function that generates all of the elements for the page
+const generatePage = () => {
+    const gameInfo = gamesList.get(document.title.toLowerCase());
+    document.getElementsByTagName("img")[0].setAttribute("src",`./images/voidcageBanner.png`);
+}
+
+generatePage();
