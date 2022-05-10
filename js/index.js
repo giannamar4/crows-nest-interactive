91dcd41720aec40e036a255a7188e1ded469d6c5
JustinMelore <88466579+JustinMelore@users.noreply.github.com>
2022-05-09 17:01:41 -0700

    created a script that will be used on the homepage to make the banner shift

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//function that makes the banner alternate between the different games
+function changeBanner() {
+    let interval = setInterval(() => {
+        console.log("hello");
+    }, 1000);
+}
+
+// window.addEventListener("DOMContentLoaded",changeBanner);
