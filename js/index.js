c03016dd341e4efb1d2cc1e808bcca27a4197507
giannamar4 <90292508+giannamar4@users.noreply.github.com>
2022-05-10 18:08:17 -0700

    home page carousel and layputs

--- js/index.js
+++ js/index.js
@@ -1,8 +1,48 @@
 //function that makes the banner alternate between the different games
-function changeBanner() {
-    let interval = setInterval(() => {
-        console.log("hello");
-    }, 1000);
-}
+// function changeBanner() {
+//     let interval = setInterval(() => {
+//         console.log("hello");
+//     }, 1000);
+// }
 
 // window.addEventListener("DOMContentLoaded",changeBanner);
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+ 
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+function showSlides() {
+  let j;
+  let slides = document.getElementsByClassName("slides");
+  for (j = 0; j < slides.length; j++) {
+    slides[j].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); 
+}
