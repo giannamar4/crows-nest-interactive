adf5e61815aa3182b1e7178202df1306f5b1dd5d
giannamar4 <90292508+giannamar4@users.noreply.github.com>
2022-05-12 18:20:52 -0700

    setting up gane page

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-//function that makes the banner alternate between the different games
-// function changeBanner() {
-//     let interval = setInterval(() => {
-//         console.log("hello");
-//     }, 1000);
-// }
-
-// window.addEventListener("DOMContentLoaded",changeBanner);
 
 let slideIndex = 1;
 showSlides(slideIndex);
