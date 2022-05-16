d11b65f58e454dae0a0293409b19ce4cebdfbb47
JustinMelore <88466579+JustinMelore@users.noreply.github.com>
2022-05-16 16:39:25 -0700

    Redid the code for the changing border on the homepage

--- js/index.js
+++ js/index.js
@@ -1,37 +1,63 @@
 
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slides");
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("slides");
  
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
 
-  slides[slideIndex-1].style.display = "block";
-  // dots[slideIndex-1].className += " active"; What is this?
-  slideIndex++;
+//   slides[slideIndex-1].style.display = "block";
+//   // dots[slideIndex-1].className += " active"; What is this?
+//   slideIndex++;
+// }
+
+//Allows the user to manually switch between banners
+var currentSlide = 0;
+const switchSlides = (amount) => {
+  const slides = document.getElementsByClassName("slides");
+  lastSlide = currentSlide;
+  currentSlide += amount;
+  if(currentSlide >= slides.length) {
+    currentSlide = 0;
+  }else if(currentSlide < 0) {
+    currentSlide = slides.length-1;
+  }
+  slides[lastSlide].style.opacity = 0;
+  slides[currentSlide].style.opacity = 1;
 }
 
 //Allows the carousel to switch on its own
-let interval;
+var interval;
 const startInterval = () => {
-  interval = setInterval(() => {plusSlides(1);}, 2000);
+  interval = setInterval(() => {switchSlides(1)}, 5000);
 }
 
 const stopInterval = () => {clearInterval(interval);}
-startInterval();
+window.addEventListener("DOMContentLoaded",startInterval);
+
+document.getElementsByClassName("prev")[0].addEventListener("mousedown",()=>{
+  switchSlides(-1);
+  stopInterval();
+  startInterval();
+});
+document.getElementsByClassName("next")[0].addEventListener("mousedown",()=>{
+  switchSlides(1);
+  stopInterval();
+  startInterval();
+})
